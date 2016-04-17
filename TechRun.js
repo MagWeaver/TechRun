c2e5baa01fb7c1b106e823b9afa222ba047a791a
Chris Weaver <cmagweaver@gmail.com>
2016-04-16 18:05:31 -0700

    Test

--- TechRun.js
+++ TechRun.js
@@ -4,10 +4,12 @@
 
 var hrr16 = {
 	name: 'Daniel Ramos',
+<<<<<<< HEAD
+<<<<<<< HEAD
 	name: 'Eric Smith',
   name: 'Nick Randall',
   name: 'Neekon Etemad',
-  name: 'Mike Cruz',
+  ',ik',: e Cr',',
   nam   '  son',
   name: 'Eduard Bardaji',
   name: 'Josh Summerhays',
@@ -15,7 +17,13 @@ var hrr16 = {
   name: 'Chris Weaver'  ,
   name: 'Leo',
   name: 'I can type now - Gabe'
-
+  'test'
 
 
 }
+=======
+}
+>>>>>>> 177c822729fc08a5c4ac2d2f02474eeff75ece34
+=======
+}
+>>>>>>> 177c822729fc08a5c4ac2d2f02474eeff75ece34
